f8858e2c0203ead12d34456aaed7bab59df7f292
Necmttn <necmettin.karakaya@gmail.com>
2017-03-12 11:51:28 +0800

    [DONE] evenFibonnaci Sequence

---
+++ rust/euler/evenFibonnaci/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let mut a :i32 = 1;
+    let mut b :i32 = 1;
+    let mut sum :i32 = 0;
+
+    while a < 4000000 {
+        if a % 2 == 0 {
+            sum = sum + a 
+        }
+        let c = a + b;
+        a = b;
+        b = c;
+    }
+    println!("Sum: {}", sum)
+}
