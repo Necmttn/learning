4f804bdf8081710fa7f503c80cdcdf266f3ac270
Necmttn <necmettin.karakaya@gmail.com>
2017-04-15 23:24:33 +0800

    Renamed module names for modulerity

---
+++ python/algorithms-in-python/r-1/main/five.py
@@ -0,0 +1,6 @@
+"""
+Give a single command that computes the sum from Exercise R-1.4, rely- ing on Python’s comprehension syntax and the built-in sum function.
+"""
+from four import squeareThemAll
+
+print(squeareThemAll(5))
--- python/algorithms-in-python/r-1/main/four.py
+++ python/algorithms-in-python/r-1/main/four.py
@@ -2,19 +2,11 @@
 Write a short Python function that takes a positive integer n and returns the sum of the squares of all the positive integers smaller than n.
 """
 
-print("takes a positive integer n and returns the sum of the squares of all the positive integers smaller than n")
-print("What's n ? ")
-m = int(input())
-
-
-
 def squeareThemAll(n):
     result = 0
     for number in range(n):
         result += number ** 2
-    return result
+    return print(result)
 
-
-print(squeareThemAll(m))
-
-
+def hello():
+    print("world")
