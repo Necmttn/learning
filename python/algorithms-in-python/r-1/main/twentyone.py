c73d1e45d682a0677935d71229020ced6343a3e4
Necmttn <necmettin.karakaya@gmail.com>
2017-04-16 16:46:19 +0800

    [DONE] learned about error handling

---
+++ python/algorithms-in-python/r-1/main/twentyone.py
@@ -0,0 +1,10 @@
+"""
+Write a Python program that repeatedly reads lines from standard input until an EOFError is raised, and then outputs those lines in reverse order (a user can indicate end of input by typing ctrl-D).
+"""
+array = []
+while True:
+    try:
+        tmp = input()
+        array.append(tmp)
+    except (EOFError):
+        print(array[::-1])
